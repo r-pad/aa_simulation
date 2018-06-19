16d54ee578a470154c1c555ef0a9b2421dae1277
Edward Ahn <edhyah@gmail.com>
2018-06-19 14:41:48 -0400

    Initial commit

---
+++ Model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: edwardahn
+"""
+
+import numpy as np
+
+
+class VehicleModel(object):
+    """
+    Vehicle modeled as a three degrees of freedom bicycle model
+    with brush tire model for tire dynamics.
+    """
+
+    def __init__(self):
+        """
+        Initialize model parameters.
+        """
+        pass
