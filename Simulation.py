30244cfd7e3bc953532f6fab5745111463259d66
Edward Ahn <edhyah@gmail.com>
2018-06-19 18:35:59 -0400

    Added file handling simulation graphics

---
+++ Simulation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: edwardahn
+"""
+
+import matplotlib.pyplot as plt
+
+
+class CarSimulation(object):
+    """
+    Object that controls visualization of simulation.
+    """
+
+    def __init__(self):
+        """
+        Initialize simulation parameters.
+        """
+        pass
+
+
+    def update(self, X):
+        """
+        Update visualization using new state.
+        """
+        pass
