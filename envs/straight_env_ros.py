334f5e447a132902768d70474da413a61c7bce25
Edward Ahn <edhyah@gmail.com>
2019-06-16 21:41:33 -0400

    Randomize starting yaw and distance in ROS training
    This introduces the following changes:
    - In the get_initial_state(), a target line is picked at a random distance
      in the [-2.5, 2.5] interval and random yaw in the [-pi/3, pi/3] interval
      from the vehicle's current position.
    - A coordinate transformation is performed to record the current state
      in the frame of the target straight line, which in the transformed
      frame is y=0.
    
    TODO: Randomization range limits should be configurable

--- envs/straight_env_ros.py
+++ envs/straight_env_ros.py
@@ -15,6 +15,7 @@ from rllab.envs.base import Step
 
 from aa_simulation.envs.straight_env import StraightEnv
 from aa_simulation.misc.transformations import euler_from_quaternion
+from aa_simulation.misc.utils import rotate_state, translate_state
 
 
 class StraightEnvROS(StraightEnv):
@@ -23,7 +24,7 @@ class StraightEnvROS(StraightEnv):
     line trajectory with ROS.
     """
 
-    def __init__(self, target_velocity, dt, model_type):
+    def __init__(self, target_velocity, dt, model_type, robot_type):
         """
         Initialize super class, parameters, ROS-related modules.
         """
@@ -37,18 +38,26 @@ class StraightEnvROS(StraightEnv):
         # Reward function parameters
         self._lambda1 = 0.25
 
+        # State in frame of target straight line. See get_initial_state()
+        # function for info.
+        self._current_state = np.zeros(6)
+
         # Target line to follow. See get_initial_state() function for info.
-        self.target_y = 0
+        self._target_y = 0
+        self._target_yaw = 0
 
         # Initialize nodes and set up ROS topics
         rospy.init_node('rl_planner')
         self.publisher = rospy.Publisher('commands/keyboard',
                 ackermann_msgs.msg.AckermannDriveStamped, queue_size=1)
         rospy.Subscriber('ekf_localization/odom', nav_msgs.msg.Odometry,
-                self.odometry_callback)
+                self._odometry_callback)
+        self._sensor_stamp = rospy.Time.now()
 
-        self._action_published = False
-        self.current_state = np.zeros(6)
+        # Wait this number of timesteps before computing another action (this
+        # is similar to setting a larger dt during training)
+        self._num_states_needed = 3
+        self._num_states_received = 0
 
 
     @property
@@ -57,11 +66,31 @@ class StraightEnvROS(StraightEnv):
         Get initial state of car when simulation is reset.
 
         To make training from scratch easier, set the target straight line to
-        be located not on y=0 but rather at y=self.current_state[1] which is
-        the current y-value.
+        a random straight line, which is chosen by adding some random noise
+        to the robot's current yaw, and adding a random y translation. Now, we
+        view this line from a reference frame that transforms this line to be
+        y=0. The variable self._current_state is the state of the robot in this
+        new reference frame.
         """
-        self.target_y = self.current_state[1]
-        return self.current_state
+        # Create private copy of state to avoid race conditions
+        state = np.array(self._current_state)
+
+        # Transform state back to inertial frame
+        state = rotate_state(translate_state(state, [0, self._target_y])),
+                self._target_yaw)
+
+        # Choose random target yaw value based on current yaw
+        self._target_yaw = state[2] + np.random.uniform(-np.pi/6, np.pi/6)
+
+        # Choose random target y with respect to new target yaw
+        rotated_y = rotate_state(state, -self._target_yaw)[1]
+        self._target_y = rotated_y + np.random.uniform(-0.25, 0.25)
+
+        # Set current state to the robot's state in the frame of the target
+        # straight line
+        self._set_current_state(state)
+
+        return self._current_state
 
 
     def step(self, action):
@@ -76,13 +105,14 @@ class StraightEnvROS(StraightEnv):
         msg = ackermann_msgs.msg.AckermannDriveStamped()
         msg.drive.speed = action[0]
         msg.drive.steering_angle = action[1]
+        msg.header.stamp = self._sensor_stamp
         self.publisher.publish(msg)
-        self._action_published = True
 
-        # Wait for next state reading
-        while self._action_published:
-            continue
-        nextstate = self.current_state
+        # Wait for next state readings
+        self._num_states_received = self._num_states_needed
+        while self._num_states_received > 0:
+            time.sleep(0.00001)
+        nextstate = self._current_state
         next_observation = self._state_to_observation(nextstate)
         self._state = nextstate
 
@@ -98,7 +128,7 @@ class StraightEnvROS(StraightEnv):
         """
         _, y, _, x_dot, y_dot, _ = state
         velocity = np.sqrt(x_dot**2 + y_dot**2)
-        distance = y - self.target_y
+        distance = y
 
         reward = -np.absolute(distance)
         reward -= self._lambda1 * (velocity - self.target_velocity)**2
@@ -108,7 +138,8 @@ class StraightEnvROS(StraightEnv):
         info['vel'] = velocity
         return reward, info
 
-    def odometry_callback(self, odom):
+
+    def _odometry_callback(self, odom):
         """
         Callback function for odometry state updates.
         """
@@ -120,7 +151,17 @@ class StraightEnvROS(StraightEnv):
         x_dot = odom.twist.twist.linear.x
         y_dot = odom.twist.twist.linear.y
         yaw_dot = odom.twist.twist.angular.z
-        self.current_state = [x, y, yaw, x_dot, y_dot, yaw_dot]
-        self._action_published = False
-        return
+        self._set_current_state(np.array([x, y, yaw, x_dot, y_dot, yaw_dot]))
+        self._sensor_stamp = odom.header.stamp
+        if self_num_states_received > 0:
+            self._num_states_received -= 1
+
+
+    def _set_current_state(self, state):
+        """
+        Set current state to the specified state in the frame of the target
+        straight line.
+        """
+        self._current_state = translate_state(
+                rotate_state(state, -self._target_yaw), [0, -self._target_y])
 
--- misc/utils.py
+++ misc/utils.py
@@ -17,3 +17,42 @@ def normalize_angle(angle):
     if (angle >= np.pi):
         angle -= 2*np.pi
     return angle
+
+
+def rotate_state(state, angle):
+    """
+    Rotate a state [x, y, yaw, x_dot, y_dot, yaw_dot] by a specified angle
+    in radians.
+    """
+    if len(state) != 6:
+        raise ValueError('Invalid state; dimension mismatch')
+
+    x = state[0]
+    y = state[1]
+    yaw = state[2]
+    x_dot = state[3]
+    y_dot = state[4]
+    yaw_dot = state[5]
+
+    new_x = x*np.cos(angle) - y*np.sin(angle)
+    new_y = y*np.cos(angle) + y*np.sin(angle)
+    new_yaw = normalize_angle(yaw + angle)
+
+    new_state = np.array([new_x, new_y, new_yaw, x_dot, y_dot, yaw_dot])
+    return new_state
+
+
+def translate_state(state, translation):
+    """
+    Translate a state [x, y, yaw, x_dot, y_dot, yaw_dot] by a translation
+    [dx, dy].
+    """
+    if len(state) != 6:
+        raise ValueError('Invalid state; dimension mismatch')
+    if len(translation) != 2:
+        raise ValueError('Invalid translation amount; dimension mismatch')
+
+    state = np.array(state)
+    state[0] += translation[0]
+    state[1] += translation[1]
+    return state
